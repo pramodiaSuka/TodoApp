b40e995c389c41b74333cebe200ec92b0d3205d6
Pramodia Suka <71862264+pramodiaSuka@users.noreply.github.com>
2024-05-16 13:29:17 +0700

    Project Initialization

---
+++ app/src/main/java/com/maverick/todoapp/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.maverick.todoapp.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.maverick.todoapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
